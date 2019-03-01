7d18da26f10840906c0656e5398c978940381725
Nevalyashka1337 <ivan.nedelyuk@yandex.ru>
2019-03-01 14:49:40 +0300

    add isChecked selector

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { isChecked } from './selectors/auth'
 
 export class App extends Component {
 	render() {
+		console.log(this.props)
 		return (
 			<div>
 				<h1>hello react</h1>
@@ -10,4 +13,10 @@ export class App extends Component {
 	}
 }
 
-export default App
+const mapStateToProps = state => ({
+	isChecked: isChecked(state)
+})
+
+export default connect(
+	mapStateToProps
+)(App)
