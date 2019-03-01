5e1b9b47e4d3e48a0d92af96eec4380e9f62fde7
Nevalyashka1337 <ivan.nedelyuk@yandex.ru>
2019-03-01 10:14:48 +0300

    cleaning src and configuring redux & router

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'
+import { configStore, history } from './store/index'
+import { ConnectedRouter } from 'connected-react-router'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Render = Component => {
+	ReactDOM.render(
+	<Provider store={configStore()}>
+		<ConnectedRouter history={history}>
+			<Component/>
+		</ConnectedRouter>
+	</Provider>
+	, document.getElementById('root'))
+}
+
+Render(App)
+
+if ( module.hot ) {
+	module.hot.accept('./App', () => {
+		const NextApp = require('./App').default
+		Render(NextApp)
+	})
+}
---
+++ client/src/store/index.js
@@ -0,0 +1,30 @@
+import { createStore, applyMiddleware } from 'redux'
+import { createBrowserHistory } from 'history'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import createRootReducer from '../reducers/index'
+import { routerMiddleware } from 'connected-react-router'
+import thunk from 'redux-thunk'
+
+export const history = createBrowserHistory()
+
+export const configStore = () => {
+	const store = createStore(
+    createRootReducer(history),
+    composeWithDevTools(
+      applyMiddleware(
+        routerMiddleware(history),
+        thunk
+      )
+    )
+  )
+
+	if (process.env.NODE_ENV !== 'production') {
+    if (module.hot) {
+      module.hot.accept('../reducers/index', () => {
+        store.replaceReducer(createRootReducer())
+      })
+    }
+	}
+
+	return store
+}
