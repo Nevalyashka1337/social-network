a13c738ee3d267ed46df7f2dd886bff3b3d7e10e
Nevalyashka1337 <ivan.nedelyuk@yandex.ru>
2019-03-03 13:46:31 +0300

    refactor(NavBar) for auth and not auth users

--- client/src/containers/NavBar.jsx
+++ client/src/containers/NavBar.jsx
@@ -3,15 +3,25 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { isAuth } from '../selectors/auth'
 
-function NavBar(props) {
-	console.log(props)
+function NavBar({ isAuth }) {
+	const authNavBar = () => (
+		<>
+			<li className="navbar__item"><Link to="/users">users</Link></li>
+		</>
+	)
+
+	const notAuthNavBar = () => (
+		<>
+			<li className="navbar__item"><Link to="/">home</Link></li>
+			<li className="navbar__item"><Link to="/signin">signin</Link></li>
+			<li className="navbar__item"><Link to="/signup">signup</Link></li>
+		</>
+	)
+
 	return (
 		<>
 			<ul className="navbar">
-				<li className="navbar__item"><Link to="/">home</Link></li>
-				<li className="navbar__item"><Link to="/users">users</Link></li>
-				<li className="navbar__item"><Link to="/signin">signin</Link></li>
-				<li className="navbar__item"><Link to="/signup">signup</Link></li>
+				{ isAuth ? authNavBar() : notAuthNavBar() }
 			</ul>
 		</>
 	)
