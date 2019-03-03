40034e28fbeebe2f7574396c3b0a1cce6615e51d
Nevalyashka1337 <ivan.nedelyuk@yandex.ru>
2019-03-03 13:25:54 +0300

    refactor(api) add cookie include

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,7 +1,9 @@
 export default {
 	getUsers: () => {
 		return new Promise((resolve, reject) => {
-			fetch('http://localhost:3001/api/users')
+			fetch('http://localhost:3001/api/users', {
+				credentials: 'include'
+			})
 			.then(body => body.json()).then(res => {
 				res.success ? resolve(res.users) : reject()
 			}).catch(() => reject())
