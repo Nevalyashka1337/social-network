7ae3b43fbda673f308265b9bd7eb8f0d64997001
Nevalyashka1337 <ivan.nedelyuk@yandex.ru>
2019-03-05 14:40:22 +0300

    add description to home page

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function HomePage() {
 	return (
 		<div>
 			<h1>welcome</h1>
+			<h4>you are not logged in</h4>
+			<p>please go to <Link to='/account/signin'>/account/signin</Link> if you are registered or <Link to='/account/signup'>/account/signup</Link> if you want to register</p>
 		</div>
 	)
 }
